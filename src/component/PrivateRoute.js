f863136d3a61bf096ccb417818571e2680f97424
savaliyakajal <kajal.panthercodx@gamil.com>
2023-04-11 14:17:33 +0530

    props

--- src/component/PrivateRoute.js
+++ src/component/PrivateRoute.js
@@ -2,6 +2,7 @@
 import React, { useContext } from "react";
 import { Navigate } from "react-router-dom";
 import { AppContext } from "../App";
+
 function PrivateRoute({children}) {
   // console.log("--------children----------",children)
   const [user, setUser] = useContext(AppContext);

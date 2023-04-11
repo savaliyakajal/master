d91f3f1217cc12fd2aec3d3c84bc8c1d29f7a6f4
savaliyakajal <kajal.panthercodx@gamil.com>
2023-04-11 11:04:29 +0530

    remov log

--- src/component/PublicRoute.js
+++ src/component/PublicRoute.js
@@ -2,10 +2,10 @@ import React, { useContext } from "react";
 import { Navigate } from "react-router-dom";
 import { AppContext } from "../App";
 function PublicRoute({ children }) {
-  // console.log("--------children----------",children);
+ 
   //   const Navigate = useNavigate();
   const [user, setUser] = useContext(AppContext);
-  // console.log("----------user", user.user.email);
+ 
 
   if (user.user.email) {
     return <div> {children}</div>;
